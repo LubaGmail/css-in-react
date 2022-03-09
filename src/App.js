861f765a379dbc3537f390ba05c150b552e3c219
LL Powe <powe2009@gmail.com>
2022-03-09 08:52:45 -0500

    css in react app

---
+++ src/App.js
@@ -0,0 +1,266 @@
+import * as React from 'react';
+import axios from 'axios';
+import { sortBy } from 'lodash';
+
+import './App.css';
+
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case 'REMOVE_STORY':
+      return {
+        ...state,
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        ),
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+    'search',
+    'React'
+  );
+
+  const [url, setUrl] = React.useState(
+    `${API_ENDPOINT}${searchTerm}`
+  );
+
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer,
+    { data: [], isLoading: false, isError: false }
+  );
+
+  const handleFetchStories = React.useCallback(
+    async () => {
+      dispatchStories({ type: 'STORIES_FETCH_INIT' });
+
+      try {
+        const result = await axios.get(url);
+
+        dispatchStories({
+          type: 'STORIES_FETCH_SUCCESS',
+          payload: result.data.hits,
+        });
+      } catch {
+        dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+      }
+    }, [url]
+  );
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
+
+  const handleRemoveStory = (item) => {
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    });
+  };
+
+  const handleSearchInput = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+    event.preventDefault();
+  };
+
+  return (
+    <div className='container'>
+      <h1 className='headline-primary'>For Geeks Only</h1>
+
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
+
+      {stories.isError && <p>Something went wrong ...</p>}
+
+      {stories.isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
+      )}
+    </div>
+  );
+};
+
+const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit, }) => (
+  
+  <form className='search-form' onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+
+    <button className='button_large'
+      type="submit" 
+      disabled={!searchTerm}
+    >
+      Submit
+    </button>
+  </form>
+);
+
+const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, children, }) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id} className='label'
+      >
+        {children}
+      </label>
+ 
+      <input className='input'
+        id={id}
+        ref={inputRef}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
+
+// Object of functions
+const SORTS = {
+  NONE: (list) => list,
+  TITLE: (list) => sortBy(list, 'title'),
+  AUTHOR: (list) => sortBy(list, 'author'),
+  COMMENT: (list) => sortBy(list, 'num_comments').reverse(),
+  POINT: (list) => sortBy(list, 'points').reverse(),
+};
+
+const List = ({ list, onRemoveItem }) => {
+  const [sort, setSort] = React.useState('NONE');
+
+  //   <button type="button" onClick={() => handleSort('TITLE')}>
+  const handleSort = (sortKey) => {
+    setSort(sortKey);
+  };
+
+  // list => Object(lodash__WEBPACK_IMPORTED_MODULE_2__["sortBy"])(list, 'title')
+  const sortFunction = SORTS[sort];
+
+  const sortedList = sortFunction(list);
+
+  return (
+    <ul>
+      <li style={{ display: 'flex' }}>
+        <span style={{ width: '40%' }}>
+          <button className='button' 
+            type="button" 
+            onClick={() => handleSort('TITLE')}
+          >
+            Title
+          </button>
+        </span>
+        <span style={{ width: '30%' }}>
+          <button className='button'
+            type="button" 
+            onClick={() => handleSort('AUTHOR')}
+          >
+            Author
+          </button>
+        </span>
+        <span style={{ width: '10%' }}>
+          <button className='button'
+            type="button" 
+            onClick={() => handleSort('COMMENT')}
+          >
+            Comments
+          </button>
+        </span>
+        <span style={{ width: '10%' }}>
+          <button className='button'
+            type="button" 
+            onClick={() => handleSort('POINT')}
+          >
+            Points
+          </button>
+        </span>
+        <span style={{ width: '10%' }}>Actions</span>
+      </li>
+
+      {sortedList.map((item) => (
+        <Item 
+          key={item.objectID}
+          item={item}
+          onRemoveItem={onRemoveItem}
+        />
+      ))}
+    </ul>
+  );
+};
+
+const Item = ({ item, onRemoveItem }) => (
+  <li style={{ display: 'flex' }}>
+    <span style={{ width: '40%' }}>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span style={{ width: '30%' }}>{item.author}</span>
+    <span style={{ width: '10%' }}>{item.num_comments}</span>
+    <span style={{ width: '10%' }}>{item.points}</span>
+    <span style={{ width: '10%' }}>
+      <button className='button' 
+        type="button" 
+        onClick={() => onRemoveItem(item)}
+      >
+        Dismiss
+      </button>
+    </span>
+  </li>
+);
+
+export default App;
